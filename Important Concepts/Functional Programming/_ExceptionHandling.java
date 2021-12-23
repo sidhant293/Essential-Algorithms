983a4bb29741eb6b65f32a50e857bff375ed6328
Sidhant <56150322+sidhant293@users.noreply.github.com>
2021-12-23 13:59:01 +0530

    Create _ExceptionHandling.java

---
+++ Important Concepts/Functional Programming/_ExceptionHandling.java
@@ -0,0 +1,62 @@
+/*
+Suppose we have a Biconsumer which takes two numbers and prints their division. Now if an exception occures, where should we handle that exception 
+*/
+
+public class _ExceptionHandling {
+    public static void main(String[] args) {
+
+      BiConsumer<Integer,Integer> biConsumer= (a,b)-> System.out.println(a/b);
+      biConsumer.accept(3/2);
+        
+    }
+}
+
+/*
+One thing we can do is that implement try catch inside lambda. 
+But that will be a contradiction to one line expression
+*/
+
+public class _ExceptionHandling {
+    public static void main(String[] args) {
+
+      BiConsumer<Integer,Integer> biConsumer= (a,b)-> {
+        try{
+          System.out.println(a/b);
+        }catch(Exception e){
+          System.out.println("exception occured");
+        }
+      }
+      biConsumer.accept(3/0);
+        
+    }
+}
+
+/*
+To handle exceptions correctly we can wrap one lambda inside another
+*/
+
+public class _ExceptionHandling {
+    public static void main(String[] args) {
+
+      BiConsumer<Integer,Integer> biConsumer= (a,b)-> {
+        try{
+          System.out.println(a/b);
+        }catch(Exception e){
+          System.out.println("exception occured");
+        }
+      }
+      
+      wrapperLambda(biConsumer).accept(3/0);        
+    }
+  
+    private static BiConsumer<Integer,Integer> wrapperLambda(BiConsumer<Integer,Integer> biConsumer){
+      return (a,b)->{
+        try{
+          biConsumer.accept(a,b);
+        }catch(Exception e){
+          System.out.println("exception occured");
+        }
+      }
+    }
+}
+

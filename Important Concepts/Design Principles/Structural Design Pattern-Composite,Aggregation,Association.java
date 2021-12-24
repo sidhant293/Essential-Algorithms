ab3a4ddc18429e0f19a7bdc9d868f5893087f84a
Sidhant <56150322+sidhant293@users.noreply.github.com>
2021-12-24 12:52:09 +0530

    Update and rename Composite,Aggregation,Association.java to Structural Design Pattern-Composite,Aggregation,Association.java

--- Important Concepts/Design Principles/Structural Design Pattern-Composite,Aggregation,Association.java
+++ Important Concepts/Design Principles/Structural Design Pattern-Composite,Aggregation,Association.java
@@ -1,5 +1,6 @@
 /*
-Composite Design Pattern is a Structural Design Pattern 
+Composite Design Pattern
+Strong Association. Entities can't exist independently
 It imporves strucutre and readability of code
 It has composite and leaf nodes.
 Leaf nodes calculate result directly , composite ndoes forward request to leaves
@@ -33,3 +34,21 @@ class CPU implements Component{
   calculatePrice(){} // price of CPU will be calculated using its childern
   
 }
+//***********************************************************************************************************************************************
+
+/*
+  Association
+  Relationship between two classes.
+  1-1,1-Many,Many-1,Many-Many
+*/
+
+//***********************************************************************************************************************************************
+
+/*
+Aggrigation
+Weak Association. Entities can exist independently.
+Aggrigation depicts "has-a" relationship. 
+A has B. Cart has products.
+Cart and products are independent entities but still has a relationship
+*/
+
